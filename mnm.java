985188103ac3c41f6c35f20b36a575b2c0d5f0d7
shafiquefarooqui <161753351+shafiquefarooqui@users.noreply.github.com>
2024-03-04 19:59:58 +0530

    Add files via upload

---
+++ mnm.java
@@ -0,0 +1,18 @@
+class Memo
+{
+   public static void main(String[] args)
+   
+   {
+   
+       System.out.print("hello ji ");
+   int a=20, b=30, C=10;
+   
+   //System.out.print(a+b);
+   String name= "shafique Farooqui";
+     
+   System.out.print("my name is"+name );
+   
+   }
+
+
+}
